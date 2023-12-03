4b5eda2fcca1e1b15ad65790a6ad0b8c6a83e9bc
Agrin <agrins@metropolia.fi>
2023-12-03 15:56:18 +0200

    inij

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,15 +9,6 @@ const Footer = () => {
         <img src="sahko-dominus.png" alt="Your Logo" className='logo-footer' loading='lazy' />
         <p>Mestari sähköisissä tilanteissa.</p>
         </div>
-        <div className="links-container">
-          <h2>Linkit</h2>
-          <ul className="links">
-            <li><a href="/">Koti</a></li>
-            <li><a href="/Palvelut">Palvelut</a></li>
-            <li><a href="/Hinnastot">Hinnastot</a></li>
-            <li><a href="/Yhteystiedot">Yhteystiedot</a></li>
-          </ul>
-        </div>
         <div className="contact-container">
           <h2>Ota yhteyttä</h2>
           <p>Diaco@SähköDominus.com</p>
