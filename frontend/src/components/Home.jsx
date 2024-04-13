b5664cedebc210a4299da9144040a281a8703075
agrin <agrin.sadon@metropolia.fi>
2024-04-13 20:05:01 +0300

    added z indez to the navlinks

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -18,11 +18,10 @@ const Home = () => {
 
   useEffect(() => {
     const coverTimeout = setTimeout(() => {
-      setShowCover(false);
-    }, 2000);
-
-    const logoTimeout = setTimeout(() => {
       setShowLogo(false);
+      setTimeout(() => {
+        setShowCover(false);
+      }, 1000); // adjust the delay according to your slide animation duration
     }, 1900);
 
     const intervalId = setInterval(() => {
@@ -31,44 +30,42 @@ const Home = () => {
 
     return () => {
       clearTimeout(coverTimeout);
-      clearTimeout(logoTimeout);
       clearInterval(intervalId);
     };
   }, [images]);
 
   return (
-    <div>
-      <div className='screen'>
-        <Navbar showLinks={showLinks} setShowLinks={setShowLinks} />
-        <div className="cover" style={{ height: showCover ? '100vh' : '0' }}>
-          <img src="/sahkodominus-logo.png" alt="Logo" className="logo-cover" style={{ opacity: showLogo ? 1 : 0 }} />
-        </div>
-        <div className="cover-small" style={{ height: showCover ? '100vh' : '0' }}>
-          <img src="/sahkodominus-logo.png" alt="Logo" className="logo-cover-small" style={{ opacity: showLogo ? 1 : 1 }} />
-        </div>
-        <div className="home">
-          <div className='home-pic-frame' style={{ backgroundImage: `url(${images[currentImage]})` }} loading="lazy" decoding="async">
-            <div className='home-main'>
-              <h1>Mestari sähköisissä tilanteissa</h1>
-              <h2>Varaa maksuton kartoitus!</h2>
-              <div className="buttons-container-home">
-              <ScrollLink to="contact-section" smooth={true} duration={500} offset={-180}>
-                <button className="cta1-button">Ota yhteyttä</button>
-              </ScrollLink>
-              <ScrollLink to="pricing-section" smooth={true} duration={500} offset={-240}>
-                <button className="cta2-button">Hinnastot</button>
-              </ScrollLink>
+      <div>
+        <div className='screen'>
+          <Navbar showLinks={showLinks} setShowLinks={setShowLinks} />
+          <div className="cover" style={{ height: showCover ? '100vh' : '0', transition: 'height 1s ease' }}>
+            {showCover && (
+                <img src="/sahkodominus-logo.png" alt="Logo" className="logo-cover" style={{ opacity: showLogo ? 1 : 0 }} />
+            )}
+          </div>
+          <div className="home">
+            <div className='home-pic-frame' style={{ backgroundImage: `url(${images[currentImage]})` }} loading="lazy" decoding="async">
+              <div className='home-main'>
+                <h1>Mestari sähköisissä tilanteissa</h1>
+                <h2>Varaa maksuton kartoitus!</h2>
+                <div className="buttons-container-home">
+                  <ScrollLink to="contact-section" smooth={true} duration={500} offset={-180}>
+                    <button className="cta1-button">Ota yhteyttä</button>
+                  </ScrollLink>
+                  <ScrollLink to="pricing-section" smooth={true} duration={500} offset={-240}>
+                    <button className="cta2-button">Hinnastot</button>
+                  </ScrollLink>
+                </div>
               </div>
             </div>
           </div>
+          <Element name="service"> <Service /> </Element>
+          <Element name="pricing-section"> <Pricing /> </Element>
+          <Element name="Meistä"> <AboutUs /> </Element>
         </div>
-        <Element name="service"> <Service /> </Element>
-        <Element name="pricing-section"> <Pricing /> </Element>
-        <Element name="Meistä"> <AboutUs /> </Element>
+        <Element name="contact-section"> <Contact /> </Element>
+        <Footer/>
       </div>
-      <Element name="contact-section"> <Contact /> </Element>
-      <Footer/>
-    </div>
   );
 };
 
