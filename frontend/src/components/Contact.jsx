6315b7080d39c228e7432a6ce6aa458fa50b1b2e
agrin <agrin.sadon@metropolia.fi>
2024-04-13 19:22:31 +0300

    changed the location of contact button to middle

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -66,7 +66,7 @@ const Contact = () => {
   return (
       <div>
         <h1 className="contact-header">Ota yhteyttä!</h1>
-        <p className="contact-text">Meille voit kirjoittaa milloin tahansa! <br/> Varmistamme, että saat vastauksen 24 tunnin sisällä.</p>
+        <p className="contact-text">Meille voit kirjoittaa milloin tahansa! <br/> Saat vastauksen 24 tunnin sisällä.</p>
         <div className="contact">
           <div className="contact-square">
             <div className="input-group-row">
