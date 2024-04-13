ab01e058f2161138aaf7821173547ef5d1c36baa
agrin <agrin.sadon@metropolia.fi>
2024-04-13 19:14:05 +0300

    changed the location of contact button to middle

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -33,134 +33,134 @@ const Contact = () => {
       },
       body: JSON.stringify(formData),
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Request failed with status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setFormData({
-          name: '',
-          email: '',
-          phone: '',
-          address: '',
-          postcode: '',
-          city: '',
-          message: '',
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Request failed with status ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setFormData({
+            name: '',
+            email: '',
+            phone: '',
+            address: '',
+            postcode: '',
+            city: '',
+            message: '',
+          });
+          setIsMessageSent(true);
+          setTimeout(() => {
+            setIsMessageSent(false);
+          }, 4000);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+        .finally(() => {
+          setIsSending(false);
         });
-        setIsMessageSent(true);
-        setTimeout(() => {
-          setIsMessageSent(false);
-        }, 4000);
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-      .finally(() => {
-        setIsSending(false);
-      });
   };
 
   return (
-    <div>
-      <h1 className="contact-header">Ota yhteyttä!</h1>
-      <p className="contact-text">Meille voit kirjoittaa milloin tahansa! <br/> Varmistamme, että saat vastauksen 24 tunnin sisällä.</p>
-      <div className="contact">
-        <div className="contact-square">
-          <div className="input-group-row">
-            <div className="input-group">
-              <label htmlFor="name">Nimi</label>
-              <input type="text" id="name" placeholder="Syötä nimi" value={formData.name} onChange={handleInputChange} />
-            </div>
-            <div className="input-group">
-              <label htmlFor="email">Sähköposti</label>
-              <input type="text" id="email" placeholder="Syötä sähköposti" value={formData.email} onChange={handleInputChange} />
-            </div>
-          </div>
-
-          <div className="input-group-row">
-            <div className="input-group">
-              <label htmlFor="phone">Puhelin</label>
-              <input type="text" id="phone" placeholder="Syötä puhelinnumero" value={formData.phone} onChange={handleInputChange} />
-            </div>
-            <div className="input-group">
-              <label htmlFor="address">Osoite</label>
-              <input type="text" id="address" placeholder="Syötä Osoite" value={formData.address} onChange={handleInputChange} />
-            </div>
-          </div>
-
-          <div className="input-group-row">
-            <div className="input-group">
-              <label htmlFor="postcode">Postinumero</label>
-              <input type="text" id="postcode" placeholder="Syötä postinumero" value={formData.postcode} onChange={handleInputChange} />
-            </div>
-            <div className="input-group">
-              <label htmlFor="city">Kaupunki</label>
-              <input type="text" id="city" placeholder="Syötä kaupunki" value={formData.city} onChange={handleInputChange} />
-            </div>
-          </div>
-
-          <div className="input-group">
-            <label htmlFor="message">Viesti</label>
-            <textarea type="text" id="message" placeholder="Syötä viesti..." value={formData.message} onChange={handleInputChange} />
-          </div>
-
-          <div className="button-with-message">
-            <button className={`button-contact ${isSending ? 'sending' : ''}`} onClick={handleSubmit}>
-              <span>{isSending ? 'Lähetetään...' : 'Lähetä'}</span>
-              <FontAwesomeIcon icon={faArrowRight} />
-            </button>
-            {isMessageSent && (
-              <div className="message-sent">
-                <FontAwesomeIcon icon={faCheckCircle} className="message-icon" />
-                <p>Viesti Lähetetty!</p>
+      <div>
+        <h1 className="contact-header">Ota yhteyttä!</h1>
+        <p className="contact-text">Meille voit kirjoittaa milloin tahansa! <br/> Varmistamme, että saat vastauksen 24 tunnin sisällä.</p>
+        <div className="contact">
+          <div className="contact-square">
+            <div className="input-group-row">
+              <div className="input-group">
+                <label htmlFor="name">Nimi</label>
+                <input type="text" id="name" placeholder="Syötä nimi" value={formData.name}
+                       onChange={handleInputChange}/>
               </div>
-            )}
+              <div className="input-group">
+                <label htmlFor="email">Sähköposti</label>
+                <input type="text" id="email" placeholder="Syötä sähköposti" value={formData.email}
+                       onChange={handleInputChange}/>
+              </div>
+            </div>
+
+            <div className="input-group-row">
+              <div className="input-group">
+                <label htmlFor="phone">Puhelin</label>
+                <input type="text" id="phone" placeholder="Syötä puhelinnumero" value={formData.phone}
+                       onChange={handleInputChange}/>
+              </div>
+              <div className="input-group">
+                <label htmlFor="address">Osoite</label>
+                <input type="text" id="address" placeholder="Syötä Osoite" value={formData.address}
+                       onChange={handleInputChange}/>
+              </div>
+            </div>
+
+            <div className="input-group-row">
+              <div className="input-group">
+                <label htmlFor="postcode">Postinumero</label>
+                <input type="text" id="postcode" placeholder="Syötä postinumero" value={formData.postcode}
+                       onChange={handleInputChange}/>
+              </div>
+              <div className="input-group">
+                <label htmlFor="city">Kaupunki</label>
+                <input type="text" id="city" placeholder="Syötä kaupunki" value={formData.city}
+                       onChange={handleInputChange}/>
+              </div>
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="message">Viesti</label>
+              <textarea type="text" id="message" placeholder="Syötä viesti..." value={formData.message}
+                        onChange={handleInputChange}/>
+            </div>
+
+            <button className={`button-contact ${isSending ? 'sending' : ''}`} onClick={handleSubmit}>
+              <span>{isSending ? 'Lähetetään...' : isMessageSent ? 'Lähetetty' : 'Lähetä'}</span>
+              {isMessageSent && <FontAwesomeIcon icon={faCheckCircle} className="message-icon"/>}
+            </button>
+
+          </div>
+        </div>
+        <div className="contact-template">
+          <div className="template-box">
+            <div className="template-circle">
+              <a href="mailto:Diako@sahkodominus.info">
+                <FontAwesomeIcon icon={faEnvelope} className="template-logo" />
+              </a>
+            </div>
+            <div>
+              <h4>Sähköposti</h4>
+              <p>Diako@sahkodominus.info</p>
+            </div>
+          </div>
+
+          <div className="template-box">
+            <div className="template-circle">
+              <a href="tel:+358505402222">
+                <FontAwesomeIcon icon={faPhone} className="template-logo" />
+              </a>
+            </div>
+            <div>
+              <h4>Puhelinnumero</h4>
+              <p>0505402222</p>
+            </div>
+          </div>
+
+          <div className="template-box">
+            <div className="template-circle">
+              <FontAwesomeIcon icon={faGlobe} className="template-logo" />
+            </div>
+            <div className="social-icons-container">
+              <a href="https://www.facebook.com/profile.php?id=61554477816227" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faFacebook} className="social-icon" />
+              </a>
+              <a href="https://www.instagram.com/sahkodominus/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faInstagram} className="social-icon" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
-      <div className="contact-template">
-        <div className="template-box">
-          <div className="template-circle">
-            <a href="mailto:Diako@sahkodominus.info">
-              <FontAwesomeIcon icon={faEnvelope} className="template-logo" />
-            </a>
-          </div>
-          <div>
-            <h4>Sähköposti</h4>
-            <p>Diako@sahkodominus.info</p>
-          </div>
-        </div>
-
-        <div className="template-box">
-          <div className="template-circle">
-            <a href="tel:+358505402222">
-              <FontAwesomeIcon icon={faPhone} className="template-logo" />
-            </a>
-          </div>
-          <div>
-            <h4>Puhelinnumero</h4>
-            <p>0505402222</p>
-          </div>
-        </div>
-
-        <div className="template-box">
-          <div className="template-circle">
-            <FontAwesomeIcon icon={faGlobe} className="template-logo" />
-          </div>
-          <div className="social-icons-container">
-            <a href="https://www.facebook.com/profile.php?id=61554477816227" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faFacebook} className="social-icon" />
-            </a>
-            <a href="https://www.instagram.com/sahkodominus/" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faInstagram} className="social-icon" />
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
   );
 };
 
