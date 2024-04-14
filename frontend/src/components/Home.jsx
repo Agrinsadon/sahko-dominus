6d0d61c0e4daeee8470b3a99a6fa7e1e3d797057
agrin <agrin.sadon@metropolia.fi>
2024-04-15 00:46:43 +0300

    added the logo back

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -43,6 +43,9 @@ const Home = () => {
                 <img src="/sahkodominus-logo.png" alt="Logo" className="logo-cover" style={{ opacity: showLogo ? 1 : 0 }} />
             )}
           </div>
+          <div className="cover-small" style={{ height: showCover ? '100vh' : '0' }}>
+            <img src="/sahkodominus-logo.png" alt="Logo" className="logo-cover-small" style={{ opacity: showLogo ? 1 : 1 }} />
+          </div>
           <div className="home">
             <div className='home-pic-frame' style={{ backgroundImage: `url(${images[currentImage]})` }} loading="lazy" decoding="async">
               <div className='home-main'>
